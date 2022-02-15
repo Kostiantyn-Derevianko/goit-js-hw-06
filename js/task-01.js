6bf3077fbe0ad3bbb7c25cc3e3cd79e608a45fd6
Kostiantyn Derevianko <derkos76@gmail.com>
2022-02-15 16:36:55 +0200

    transf home 150222

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,5 @@
 const list = document.querySelectorAll('.item');
 console.log(list);
 console.log("Number of categories:", list.length);
+const firstEl = list[0];
+console.log(firstEl.querySelector("h2").textContent);
