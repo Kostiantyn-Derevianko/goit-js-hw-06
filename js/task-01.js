89569fdf0304546082cd152b5197147abd6205bd
Kostiantyn Derevianko <derkos76@gmail.com>
2022-02-17 08:35:09 +0200

    transf work 170222

--- js/task-01.js
+++ js/task-01.js
@@ -3,3 +3,11 @@ console.log(list);
 console.log("Number of categories:", list.length);
 const firstEl = list[0];
 console.log(firstEl.querySelector("h2").textContent);
+
+const button = document.querySelector(".btn");
+
+button.style.backgroundColor = "teal";
+button.style.fontSize = "24px";
+button.style.textAlign = "center";
+
+console.log(button.style);
